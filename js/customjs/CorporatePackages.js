3e55545f73d27178144b10787255d9cfd9f5e43e
rajesh rawat <rajesh.rawat@smopl.com>
2018-06-05 17:57:33 +0530

    commit

--- js/customjs/CorporatePackages.js
+++ js/customjs/CorporatePackages.js
@@ -1715,7 +1715,7 @@ function bulkCandidateActions(p_action_type) {
         l_final_map.examName = $('#i_exam_name').val();
         l_final_map.inputs = l_map;
         
-        alert(JSON.stringify(l_final_map));
+        //alert(JSON.stringify(l_final_map));
         $(".loading").show();
         $.ajax({
             url: '/common/bulk-actions',
